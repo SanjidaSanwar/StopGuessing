aa945056df830284a43dddcb0b4d847ae1e66c0c
SanjidaSanwar <sanjidasanwar@cmail.carleton.ca>
2020-03-19 21:05:20 -0400

    Add files via upload
    stopguessing-2

---
+++ StopGuessing/EncryptionPrimitives/ManagedSHA256.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+
+namespace StopGuessing.EncryptionPrimitives
+{
+    public static class ManagedSHA256
+    {
+        [ThreadStatic] static SHA256 Sha256;
+
+
+        public static byte[] Hash(byte[] buffer)
+        {
+            if (Sha256 == null)
+            {
+                Sha256 = SHA256.Create();
+            }
+            //using (SHA256 hash = SHA256.Create())
+            //{
+            return Sha256.ComputeHash(buffer);
+            //}
+
+        }
+
+    }
+}
